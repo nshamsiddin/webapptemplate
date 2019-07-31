472edff5321d1de601c0d7a015ec7d86606658e2
Shamsiddin Nabiev <shamsiddin@live.ru>
2019-07-31 15:38:59 +0500

    filename fix

--- modules/hiveinterface.js
+++ modules/hiveinterface.js
@@ -9,14 +9,12 @@ const username = config.hive.username
 const password = config.hive.password
 const folder = config.app.folder
 
-let reports_path = __dirname + '/../reports/'
+let reports_path = path.resolve(__dirname + '/../reports/')
+logger.info(reports_path)
+
 
 exports.query = (query, name) => axios.post(config.hive.api, {
     query, url, username, password,
-    filePath: path.resolve(`${__dirname}/../test.xlsx`)
-    // filePath: path.resolve(`${__dirname}/${folder}/${name}.xlsx`)
-}, (err, result) => {
-    console.log(path.resolve(`${__dirname}/../${name}`))
-    console.log(result)
+    filePath: `${reports_path}/${name}`
 })
-    .catch((err) => logger.error(path.resolve(`${__dirname}/test.xlsx`)))
+    .catch((err) => logger.error(`${reports_path}/${name}`))
